0d4c8f9e6977b63a4916a30ba9d297fb64b27499
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-10 21:57:32 +0530

    Downward Triangle Star Pattern

---
+++ StarPattern/5.DownwardTriangleStarPattern.js
@@ -0,0 +1,14 @@
+// 5. Downward Triangle Star Pattern
+
+function downwardTriangle(n) {
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+}
+
+console.log(downwardTriangle(5));
