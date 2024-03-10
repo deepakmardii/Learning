4ebcb8fb4aaac4a290f3e423545ca69d49c708bb
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-10 20:51:50 +0530

    Square Star Pattern

---
+++ StarPattern/1.SquareStarPattern.js
@@ -0,0 +1,14 @@
+// 1. Square Star Pattern in Javascript
+
+function star(n) {
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+}
+
+console.log(star(5));
