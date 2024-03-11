ca613ed3db35be4018619d518dfd64f302a2feac
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-11 10:56:44 +0530

    Hollow Triangle Star Pattern

---
+++ StarPattern/6.HollowTriangleStarPattern.js
@@ -0,0 +1,23 @@
+// 6. Hollow Triangle Star Pattern
+
+function hollowTriangle(n) {
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (i === n) {
+        //only last row
+        string += "*";
+      } else {
+        if (j == 0 || j == i - 1) {
+          string += "*";
+        } else {
+          string += " ";
+        }
+      }
+    }
+    string += "\n";
+  }
+  console.log(string);
+}
+
+console.log(hollowTriangle(5));
